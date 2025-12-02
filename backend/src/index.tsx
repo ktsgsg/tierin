7cb3aba088905d32a61e57efa8e3df818a13fa53
Keita <184593592+ktsgsg@users.noreply.github.com>
2025-12-02 15:13:35 +0900

    シラバス検索APIの追加

--- backend/src/index.tsx
+++ backend/src/index.tsx
@@ -4,6 +4,7 @@ import { logger } from 'hono/logger'
 
 import {about} from './about/page.js'
 import { posting } from './api/posting.js'
+import { suggest } from './api/suggest.js'
 
 const app = new Hono();
 
@@ -23,6 +24,7 @@ app.get('/', (c) => {
 
 app.route('/about', about)
 app.route('/api/posting/', posting)
+app.route('/api/database/', suggest)
 
 
 serve({
