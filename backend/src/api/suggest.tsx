fcd8f311894b3f7920d8a87c199d873521d9c41f
Keita <184593592+ktsgsg@users.noreply.github.com>
2025-12-02 15:24:08 +0900

    READMEの追加

--- backend/src/api/suggest.tsx
+++ backend/src/api/suggest.tsx
@@ -21,7 +21,7 @@ suggest.get('/subject', async (c) => {
       }
    }
 
-   let { data, error } = await supabase.from('subjects').select(select).like('code', `%${code}%`).like('name', `%${name}%`).like('teachers', `%${teachers}%`);
+   const { data, error } = await supabase.from('subjects').select(select).like('code', `%${code}%`).like('name', `%${name}%`).like('teachers', `%${teachers}%`);
    if (error) {
       return c.json({ error: error.message }, 500);
    }
