a3f0553fe3f73e0c28686c7aaa7ae71d7897c0d9
sora161023 <231205104@ccmailg.meijo-u.ac.jp>
2025-12-07 23:37:23 +0900

    超簡易プレビュー表示

--- frontend/tierin/app/preview/page.tsx
+++ frontend/tierin/app/preview/page.tsx
@@ -1,19 +1,48 @@
 export default async function PreviewPage() {
-    const response = await fetch('http://api:3000/api/preview/contents?contents_id=0c93c2cd-b2a1-48f5-b635-b971589eb16c', {
+    const response = await fetch('http://api:3000/api/preview/contents?contents_id=f7ec3b35-6415-47c8-bfe2-09e70b6e2bbd', {
         cache: 'no-store', 
     });
-    // const response = {
-    //     status: 200,
-    //     json: async () => ({
-    //         message: 'This is a preview response'
-    //     })
-    // }
+
     console.log('response', response.status);
     const data = await response.json();
+    const resourceBase = 'http://localhost:3000/storage/resources/';
     return (
         <div>
-            {/* <h1>Preview Page</h1> */}
-            <pre>{JSON.stringify(data, null, 2)}</pre>
+            {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+            <h1>Preview Page</h1>
+            <h2>{data.title}</h2>
+            <p>Contents ID: {data.contents_id}</p>
+            <h3>Resource: </h3>
+            {/* {画像やPDFをここに表示} */}
+            <p>Resources:</p>
+            <ul>
+                {data.metadata.resources.map((resource: string) => {
+                    const resourceUrl = resourceBase + resource;
+                    if (resource.endsWith('.jpeg') || resource.endsWith('.jpg') || resource.endsWith('.png')) {
+                        return (
+                            <li key={resource}>
+                                <img src={resourceUrl} alt={`Resource`} width={200} />
+                            </li>
+                        );
+                    } else if (resource.endsWith('.pdf')) {
+                        return (
+                            <li key={resource}>
+                                <a href={resourceUrl} target="_blank" rel="noopener noreferrer">
+                                    View PDF {resource}
+                                </a>
+                            </li>
+                        );
+                    } else {
+                        return (
+                            <li key={resource}>
+                                <a href={resourceUrl} target="_blank" rel="noopener noreferrer">
+                                    Download Resource
+                                </a>
+                            </li>
+                        );
+                    }
+                })}
+            </ul>
         </div>
     )
 }
