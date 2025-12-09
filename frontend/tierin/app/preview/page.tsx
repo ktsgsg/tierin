47adfb4b26d8e2ad83d15ea5a583c55d03506331
sora161023 <231205104@ccmailg.meijo-u.ac.jp>
2025-12-09 16:02:24 +0900

    pdfもページ内で表示可能に変更

--- frontend/tierin/app/preview/page.tsx
+++ frontend/tierin/app/preview/page.tsx
@@ -1,3 +1,5 @@
+import styles from './page.module.css'
+
 export default async function PreviewPage(props: any) {
     const searchParams = await props.searchParams;
     const contents_id = searchParams.contents_id;
@@ -17,30 +19,29 @@ export default async function PreviewPage(props: any) {
 
     return (
         <div>
-            <h1>Content Preview</h1>
-            <h2>{data.title}</h2>
-            <p>Contents ID: {data.contents_id}</p>
-            <p>Resources</p>
-            <ul>
+            <ul className={styles.resource_list}>
                 {data.metadata.resources.map((resource: string) => {
                     const resourceUrl = resourceBase + resource;
                     if (resource.endsWith('.jpeg') || resource.endsWith('.jpg') || resource.endsWith('.png')) {
                         return (
-                            <li key={resource}>
+                            <li key={resource} className={styles.resource_item}>
                                 <img src={resourceUrl} alt={`Resource`} width={800} />
                             </li>
                         );
                     } else if (resource.endsWith('.pdf')) {
                         return (
-                            <li key={resource}>
-                                <a href={resourceUrl} target="_blank" rel="noopener noreferrer">
-                                    View PDF {resource}
-                                </a>
+                            <li key={resource} className={styles.resource_item}>
+                                <iframe
+                                    src={resourceUrl}
+                                    width="800"
+                                    height="600"
+                                    title="PDF Resource"
+                                ></iframe>
                             </li>
                         );
                     } else {
                         return (
-                            <li key={resource}>
+                            <li key={resource} className={styles.resource_item}>
                                 <a href={resourceUrl} target="_blank" rel="noopener noreferrer">
                                     Download Resource
                                 </a>
